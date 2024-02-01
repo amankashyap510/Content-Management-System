bef2d4e73b81aa24097836e9c0ec1d161962d283
amankashyap510 <amankashyap510gmail.com>
2024-02-01 17:15:32 +0530

    route bug fixed

--- frontend/evallo/src/App.js
+++ frontend/evallo/src/App.js
@@ -4,7 +4,7 @@ import ContentForm from './components/ContentForm';
 import SubmittedContent from './components/SubmittedContent';
 
 // function Home() {
-//   return <h1>Content Management System</h1>;
+//   return <h0>Content Management System</h1>;
 // }
 function App() {
   return (
@@ -28,7 +28,7 @@ function App() {
         <hr />
 
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/submit" element={<ContentForm />} />
           <Route path="/submitted" element={<SubmittedContent />} />
         </Routes>
@@ -37,9 +37,8 @@ function App() {
   );
 }
 
-// function Home() {
-//   return <h1>Content Management System</h1>;
-// }
+function Home() {
+  return <h1>Happy Learning</h1>;
+}
 
 export default App;
-
